fa5c56bb9b9d5aa22da83a80ea0b1db4e723ea27
ginger <26461046+gingerchew@users.noreply.github.com>
2024-02-02 17:17:42 -0500

    Adds comments about methods and converts `name` to `type`

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 
-type Action = (ctx:Context) => void;
+/**
+ * Are Actions and Listeners the same thing?
+ * 
+ * For the time being yes. Once the idea for what `context` will be
+ * in this... context... these two will differentiate themselves.
+ */
+type Action = (ctx: State) => void;
+type Listener = (ctx: State) => void
 
-type Listener = (ctx: IteratorResult<State, void>) => void
-interface Context {
-    name: string;
+interface StateTarget {
+    target: string;
+    actions: Action[];
 }
 
+type Events = Record<string, string | StateTarget>;
+
 interface State {
-    name: string;
+    type: string;
+    on?: Events;
     enter?: Action|Action[];
     exit?: Action|Action[];
 }
@@ -27,7 +37,6 @@ function* _FSM(states:State[]) {
 		activeState = states[nextState],
         requestedState:string|undefined;
 
-
 	while (true) {
 		if (nextState >= states.length) nextState = 0;
 		
@@ -40,9 +49,10 @@ function* _FSM(states:State[]) {
 		requestedState = yield activeState;
 
         if (requestedState !== undefined) {
-            nextState = states.findIndex(state => state.name === requestedState) ?? activeState;
+            nextState = states.findIndex(state => state.type === requestedState);
+            if (nextState === UNINIT) nextState = prevState;
         } else {
-            nextState = states.findIndex(states => states.name === activeState.name) + 1;
+            nextState = states.findIndex(states => states.type === activeState.type) + 1;
         }
 
 		runActions(states[prevState]?.exit);
@@ -50,7 +60,7 @@ function* _FSM(states:State[]) {
 }
 
 function createMachine(states?:(string|State)[]) {
-    states = states?.map(state => typeof state === 'string' ? ({ name: state }) : state);
+    states = states?.map(state => typeof state === 'string' ? ({ type: state }) : state);
 	const _states = _FSM((states as State[]) ?? []);
     let _ctx = _states.next();
 	
@@ -59,34 +69,48 @@ function createMachine(states?:(string|State)[]) {
 	return {
         /** Getters */
         get active() {
-            return _ctx.value?.name ?? UNINIT
+            return _ctx.value?.type ?? UNINIT
         },
         get done() {
             return _ctx.done;
         },
-
-        /** Meta */
-        matches(stateNameToCheck:string) {
-            return stateNameToCheck === this.active;
-        },
         /** Methods */
-        dispatch(event:string) {
-            if (this.done) return;
-            
-            listeners.forEach(listener => listener(_ctx));
-        },
+        /**
+         * Add a listener that fires on every state change
+         */
         subscribe(listener:Listener) {
             listeners.add(listener);
-            listener(_ctx);
+
+            listener(_ctx.value!);
             return () => listeners.delete(listener);
         },
+        send(eventType:string) {
+            if (!_ctx.value!.on) return;
+
+            let next = _ctx.value?.on[eventType];
+
+            if ((next as StateTarget).actions?.length) {
+                (next as StateTarget).actions!.forEach(action => action(_ctx.value!))
+            }
+            this.next((next as StateTarget)?.target ?? next)
+        },
+        /**
+         * Toggle through the state machine
+         * Passing an optional state name will 
+         * go to that state instead of
+         * the next in index order
+         */
 		next(requestedState?:string) {
 			_ctx = _states.next(requestedState);
-            listeners.forEach(listener => listener(_ctx));
+            listeners.forEach(listener => listener(_ctx.value!));
 		},
+        /**
+         * Kill the state machine/generator
+         */
 		destroy() {
 			_ctx = _states.return();
-		},
+            listeners.forEach(listener => listeners.delete(listener));
+		}
 	}
 }
 
