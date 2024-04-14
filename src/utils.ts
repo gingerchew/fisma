67ea4ef65171983ff26dbe9059ebfc7a4acaa24f
ginger <26461046+gingerchew@users.noreply.github.com>
2024-04-13 23:41:42 -0400

    Adds new utils function updates runActions function
    runActions now accepts undefined actions, which it will skip
    
    adds new keys util to alias Object.keys static method

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,14 @@
 import { UnformattedState, Action, InactiveState, State } from "./types";
 
-export const createState = (state:string|UnformattedState) => Object.assign({
+export const createState = (state:UnformattedState) => Object.assign({
     enter: [],
     exit: [],
     on: {}
 }, typeof state === 'string' ? { type: state } : state)
 
-export const runActions = (actions: Action|Action[], activeState:State|InactiveState) => {
-    ([] as Action[]).concat(actions).forEach(action => action(activeState))
+export const runActions = (actions: undefined | Action | Action[], activeState: State | InactiveState) => {
+    actions && ([] as Action[]).concat(actions).forEach(action => action?.(activeState))
 };
 
+
+export const keys = Object.keys;
