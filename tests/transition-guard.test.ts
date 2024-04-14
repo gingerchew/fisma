ba870f45ac6683559f8d230d72da4d44a38b17a2
ginger <26461046+gingerchew@users.noreply.github.com>
2024-04-14 09:23:57 -0400

    Adds tests for transition guard

---
+++ tests/transition-guard.test.ts
@@ -0,0 +1,28 @@
+import { createMachine } from "../src";
+import { it, describe, expect } from 'vitest';
+
+describe('fisma/transition guard', () => {
+    it('should prevent transitions', () => {
+        let shouldTrans = false;
+        const m = createMachine({
+            states: {
+                A: {
+                    cond: () => shouldTrans
+                },
+                B: {}
+            }
+        });
+
+        m.next();
+        expect(m.current).toBe('A');
+        shouldTrans = true;
+        m.next();
+        expect(m.current).toBe('B');
+        console.log(m);
+    })
+
+
+    it('should run still??', () => {
+        expect(true).toBe(true);
+    })
+})
