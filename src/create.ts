7b062a548fe9905d83eda63ba62389999d45f8d2
ginger <26461046+gingerchew@users.noreply.github.com>
2024-04-14 09:06:43 -0400

    Adds extra dev errors and shortens default id key

--- src/create.ts
+++ src/create.ts
@@ -5,41 +5,51 @@ import { keys, runActions } from './utils';
 import { Engine } from './fsm';
 
 function createMachine<T extends Record<string, State>>(config: MachineConfig<T>):Machine<T> {
-    
+    if (import.meta.env.DEV) {
+        if(!config) throw new Error('Machine must have a config to run');
+        if (Object.keys(config.states).length === 0) throw new Error('Machine cannot be stateless');
+    }
+
     const stateKeys = keys(config.states), {
         states,
-        id = `machine-${crypto.randomUUID()}`,
+        id = `m-${crypto.randomUUID()}`,
         initial = stateKeys[0] as keyof T,
         final,
     } = config;
     
-    if (import.meta.env.DEV) {
-        if (Object.keys(states).length === 0) throw new Error('Machine cannot be stateless');
-    }
 
 	const _states = Engine<T>(states, initial, final), listeners = new Set<Listener>();
     let _ctx = _states.next();
 
 
+    /**
+     * Toggle through the state machine
+     * Passing an optional state name will 
+     * go to that state instead of
+     * the next in index order
+     */
 	const next = (requestedState?:string) => {
         // Don't transition to a nonexistent state
-        if (requestedState && stateKeys.indexOf(requestedState) === -1) return;
-        if (requestedState === _ctx.value.type) {
-            return;
+        if (
+            (requestedState && stateKeys.indexOf(requestedState) === -1)
+            ||
+            requestedState === _ctx.value.type
+        ) {
+            return
         }
-        _ctx = _states.next(requestedState);
-        listeners.forEach(listener => listener(_ctx.value!));
+        _ctx = _states.next(requestedState)
+        listeners.forEach(listener => listener(_ctx.value!))
     }
 
     return {
-        id,
+        id: id,
         initial: initial!,
         /** Getters */
         get current() {
-            return _ctx.value!.type as keyof T;
+            return _ctx.value!.type as keyof T
         },
         get done() {
-            return !!_ctx.done;
+            return !!_ctx.done
         },
         /** Methods */
         next,
@@ -48,10 +58,10 @@ function createMachine<T extends Record<string, State>>(config: MachineConfig<T>
          * returns a clean up function
          */
         subscribe(listener:Listener) {
-            listeners.add(listener);
+            listeners.add(listener)
 
-            listener(_ctx.value!);
-            return () => listeners.delete(listener);
+            listener(_ctx.value!)
+            return () => listeners.delete(listener)
         },
         send(eventType:string) {
             const state = _ctx.value as ReturnedState;
@@ -66,12 +76,6 @@ function createMachine<T extends Record<string, State>>(config: MachineConfig<T>
                 next((nextState as StateTarget)?.target ?? nextState)
             }
         },
-        /**
-         * Toggle through the state machine
-         * Passing an optional state name will 
-         * go to that state instead of
-         * the next in index order
-         */
         /**
          * Kill the state machine/generator
         */
